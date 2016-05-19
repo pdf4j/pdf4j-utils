9f9b318201d5749cd21da8f712fc0f6fac772607
Uma Bala <balaumadevi@gmail.com>
2016-05-19 00:43:38 -0400

    Initial commit

---
+++ src/main/java/org/pdf4j/util/EncodingConstants.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pdf4j.util;
+
+import java.nio.charset.Charset;
+
+/**
+ * Encoding constants.
+ */
+public class EncodingConstants
+{
+
+    /**
+     * UTF-8 Charset constant.
+     */
+    public static final Charset UTF_8 = Charset.forName( "UTF-8" );
+
+    // Special characters
+    public static final String APOS = "&apos;";
+
+    public static final String GT = "&gt;";
+
+    public static final String LT = "&lt;";
+
+    public static final String AMP = "&amp;";
+
+    public static final String QUOT = "&quot;";
+
+}
